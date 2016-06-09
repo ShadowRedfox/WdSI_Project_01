4f037fa746c58a201d911df8b45d9640b18a27f6
ShadowRedfox <SabakJ@gmail.com>
2016-06-09 12:06:16 +0200

    Gotowy projekt
    gotowy projekt

--- WdSI_Project/WdSI_Project.cpp
+++ WdSI_Project/WdSI_Project.cpp
@@ -35,10 +35,6 @@ public:
 		float a = 5 + ((7 * x) / (pow(2, 22) - 1));
 		return log10(pow(a, 3)) - log10(pow(a, 5)) + pow(cos(4 * a), 3);
 	}
-
-
-
-
 };
 
 bool porownaj(osobnik a, osobnik b)
@@ -68,9 +64,14 @@ int bin2dec(osobnik a)
 }
 int _tmain(int argc, _TCHAR* argv[])
 {
+	int z=0;
+	for (int i = 0; i < wiel_pop; i++)
+		z += i;
+	//cout << z << endl;
 	srand(time(NULL));
 	vector<osobnik> populacja;
 	vector<osobnik> populacjaNew;
+	vector<osobnik> populacjaKolo;
 	for (int i = 0; i < wiel_pop; i++)
 	{
 		bool l_bin_tmp[23];
@@ -82,7 +83,6 @@ int _tmain(int argc, _TCHAR* argv[])
 		}
 		populacja.push_back(osobnikTmp);
 	}
-
 	for (int k = 0; k < max_iter; k++)
 	{
 		for (int i = 0; i < wiel_pop; i++)
@@ -90,23 +90,39 @@ int _tmain(int argc, _TCHAR* argv[])
 			populacja[i].ocena = populacja[i].przystosowanie(bin2dec(populacja[i]));
 		}
 		sort(populacja.begin(), populacja.end(), porownaj);
-		//cout.precision(10);
+		cout.precision(6);
 		cout << k + 1 << "\tBest: " << populacja[0].ocena << "\t";
 		for (int i = 0; i < 23; i++)
 		{
 			cout << populacja[0].l_bin[i];
 		}
 		cout << "\t" << 5 + ((7 * bin2dec(populacja[0])) / (pow(2, 22) - 1)) << endl;
-
 		populacjaNew.clear();
-		int esize = 2;			//w przypadku 1 nie byloby to parzyste, wiec 2 najlepszym rozwiazaniem
+		populacjaKolo.clear();
+		int esize = 2;
 		populacjaNew.push_back(populacja[0]);
 		populacjaNew.push_back(populacja[1]);
-
+		for (int i = 0; i < wiel_pop; i++)
+		{
+			//cout << "test" << endl;
+			int rand_l = rand() % z;
+			int ru = z;
+			for (int j = 0; j < wiel_pop; j++)
+			{
+				//cout << "test" << endl;
+				if (rand_l >(ru - (wiel_pop - (j + 1))))
+				{
+					//cout << j << endl;
+					populacjaKolo.push_back(populacja[j]);
+					break;
+				}
+				ru = (ru - (wiel_pop - (j + 1)));
+			}
+		}
 		for (int i = esize; i < wiel_pop; i+=2)
 		{
-			int rand1 = rand() % podzbior_k;
-			int rand2 = rand() % podzbior_k;
+			int rand1 = rand() % (wiel_pop-1);
+			int rand2 = rand() % (wiel_pop-1);
 			int m_podzialu1 = rand() % 23;
 			int m_podzialu2 = rand() % 23;
 			if (m_podzialu1 > m_podzialu2)
@@ -118,18 +134,18 @@ int _tmain(int argc, _TCHAR* argv[])
 			osobnik tmp1, tmp2;
 			for (int j = 0; j < m_podzialu1; j++)
 			{
-				tmp1.l_bin[j] = populacja[rand1].l_bin[j];
-				tmp2.l_bin[j] = populacja[rand2].l_bin[j];
+				tmp1.l_bin[j] = populacjaKolo[rand1].l_bin[j];
+				tmp2.l_bin[j] = populacjaKolo[rand2].l_bin[j];
 			}
 			for (int j = m_podzialu1; j < m_podzialu2; j++)
 			{
-				tmp1.l_bin[j] = populacja[rand2].l_bin[j];
-				tmp2.l_bin[j] = populacja[rand1].l_bin[j];
+				tmp1.l_bin[j] = populacjaKolo[rand2].l_bin[j];
+				tmp2.l_bin[j] = populacjaKolo[rand1].l_bin[j];
 			}
 			for (int j = m_podzialu2; j < 23; j++)
 			{
-				tmp1.l_bin[j] = populacja[rand1].l_bin[j];
-				tmp2.l_bin[j] = populacja[rand2].l_bin[j];
+				tmp1.l_bin[j] = populacjaKolo[rand1].l_bin[j];
+				tmp2.l_bin[j] = populacjaKolo[rand2].l_bin[j];
 			}
 			if (rand() < prawd_mut) mutuj(tmp1);
 			if (rand() < prawd_mut) mutuj(tmp2);
@@ -138,9 +154,6 @@ int _tmain(int argc, _TCHAR* argv[])
 		}
 		swap(populacja, populacjaNew);
 	}
-
-
-	//ale one ladne byly
 	system("pause>plik");
 	return 0;
 }
